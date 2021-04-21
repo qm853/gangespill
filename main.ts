d87d7150f936ab8437030829df89c3704efcdd93
qm853 <82948942+qm853@users.noreply.github.com>
2021-04-21 14:48:12 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,18 @@ let tiere = 0
 let enere = 0
 let svar = 0
 let produkt = 0
+let faktor1 = 0
+let faktor2 = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
 input.onGesture(Gesture.Shake, function () {
     tiere = 0
     enere = 0
     svar = 0
     produkt = 0
+    faktor1 = randint(0, 5)
+    faktor2 = randint(0, 5)
+    basic.showString("" + faktor1 + "x" + faktor2 + "=")
+    produkt = faktor1 * faktor2
 })
